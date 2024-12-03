a1a5d8cd06ca232096d40c80de3a1af535345c00
DC <denise.p.cheng@gmail.com>
2024-12-02 21:06:37 -0500

    Waves 1-3 complete. Adds increase temperature, decrease temperature, update temperature background, update temperature font color, and update landscape features.

--- src/index.js
+++ src/index.js
@@ -7,11 +7,19 @@ const updateTemperatureDisplay = () => {
     tempFontColor();
 };
 
+const updateLandscape = () => {
+    const setLandscape = document.getElementById('landscape');
+    setLandscape.textContent = currentLandscape;
+    tempLandscape();
+}
+
 const addDegree = () => {
     const increaseTemp = document.getElementById('currentTempButton');
     currentTemperature += 1;
     increaseTemp.textContent = currentTemperature;
     updateTemperatureDisplay();
+    tempLandscape();
+
 };
 
 const subtractDegree = () => {
@@ -19,6 +27,7 @@ const subtractDegree = () => {
     currentTemperature -= 1;
     decreaseTemp.textContent = currentTemperature;
     updateTemperatureDisplay();
+    tempLandscape();
 };
 
 const tempBackgroundColor = () => {
@@ -52,51 +61,26 @@ const tempFontColor = () => {
     } else if (currentTemperature >= 70 && currentTemperature <= 79 ) {
         currentFontColor = '#ff7f50';
     } else if (currentTemperature >= 80) {
-        currentFontColor = '#ff1493';
+        currentFontColor = '#c71585';
     }
     setFontColor.style.color = currentFontColor;
 };
 
-const registerEventHandlers = () => {
-    const increaseTempButton = document.querySelector('#increaseTempControl');
-    increaseTempButton.addEventListener('click', addDegree);
+const tempLandscape = () => {
+    const setLandscape = document.getElementById('landscape');
+    let currentLandscape = '';
 
-    const decreaseTempButton = document.querySelector('#decreaseTempControl');
-    decreaseTempButton.addEventListener('click', subtractDegree);
+    if (currentTemperature <= 59) {
+        currentLandscape = '🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲';
+    } else if (currentTemperature >= 60 && currentTemperature <= 69 ) {
+        currentLandscape = '🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃';
+    } else if (currentTemperature >= 70 && currentTemperature <= 79 ) {
+        currentLandscape = '🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷';
+    } else if (currentTemperature >= 80) {
+        currentLandscape= '🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂';
+    }
+    setLandscape.textContent = currentLandscape;
 };
-
-document.addEventListener('DOMContentLoaded', registerEventHandlers);
-
-
-
-// const tempLandscape = () => {
-//     const setLandscape = document.getElementById('currentTempButton');
-//     let currentLandscape;
-
-//     if (currentTemperature <= 49) {
-//         currentLandscape = '#00ced1';
-//     } else if (currentTemperature >= 50 && currentTemperature <= 59 ) {
-//         currentLandscape = '#00ff7f';
-//     } else if (currentTemperature >= 60 && currentTemperature <= 69 ) {
-//         currentLandscape = '#ffd700';
-//     } else if (currentTemperature >= 70 && currentTemperature <= 79 ) {
-//         currentLandscape = '#ff7f50';
-//     } else if (currentTemperature >= 80) {
-//         currentLandscape= '#ff1493';
-//     }
-//     setLandscape.style.color = currentLandscape;
-    // ??? instead or .color we need to...
-// };
-
-/*
-Temperature (F)	Landscape
-80+	"🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂"
-70-79	"🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷"
-60-69	"🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃"
-59 or below	"🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲"
-\*
-
-
 
 const registerEventHandlers = () => {
     const increaseTempButton = document.querySelector('#increaseTempControl');
@@ -107,74 +91,3 @@ const registerEventHandlers = () => {
 };
 
 document.addEventListener('DOMContentLoaded', registerEventHandlers);
-
-/*
-
-Temperature (F)	Landscape
-80+	"🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂"
-70-79	"🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷"
-60-69	"🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃"
-59 or below	"🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲"
-
-
-
-.temperature__section button {
-    background-color: #1b69f9;
-    border: none;
-    color: white;
-    padding: 15px 32px;
-    text-align: center;
-    text-decoration: none;
-    display: inline-block;
-    font-size: 16px;
-    border-radius: 10px
-}
-
-
-    <section class="temperature__section">
-        <h2>Temperature</h2>
-        <div class="temperature__content">
-            <div class="temperature__controls">
-                <span id="increaseTempControl">⬆️</span>
-                <span id="tempValue"></span>
-                <span id="decreaseTempControl">⬇️</span>
-            </div>
-            <button id="currentTempButton">Get Realtime Temperature</button>
-        </div>
-    </section>
-
-Temperature Ranges Change Text Color
-Depending on what temperature it is, either:
-
-the temperature number changes color
-the background of the temperature changes color
-There must be at least five categories of distinguishable colors.
-
-Our solution uses the following ranges, which you may use if desired:
-
-Temperature (F)	Color
-80+	Red
-70-79	Orange
-60-69	Yellow
-50-59	Green
-49 or below	Teal
-Temperature Ranges Change Landscape
-Depending on what temperature it is, a different landscape should appear on the page.
-
-These landscapes can be anything, as long as they are visual and noticeable to any instructors. For example, the landscapes can be:
-
-A single image that appears
-ASCII art
-Text
-Changing landscapes should replace the existing landscape. There should only be one visible landscape at a time.
-
-There must be at least four landscapes.
-
-Our solution uses the following ranges, which you may use if desired:
-
-Temperature (F)	Landscape
-80+	"🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂"
-70-79	"🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷"
-60-69	"🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃"
-59 or below	"🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲"
-*/
